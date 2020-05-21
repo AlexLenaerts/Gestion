014ea4fc46c2ab8e466e177fcf68b2348744c393
al <alexandrelenaerts@gmail.com>
2020-05-21 22:42:03 +0200

    cleanup

---
+++ WebApplication3/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class ContactViewModel
+    {
+        [StringLength(100)]
+        [Required(ErrorMessage = "Le nom est requis")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Le mail est incorrect")]
+        [Required(ErrorMessage = "L'email est requis")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "L'objet du message est requis")]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
---
+++ WebApplication3/Models/article.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication3.Models
+{
+    public class article
+    {
+        public int NumberRef { get; set; }
+        public string Name { get; set; }
+        public double SellPrice { get; set; }
+        public int QuantityStock { get; set; }
+        public static bool found { get; set; }
+        public static string show { get; set; }
+    }
+
+}
